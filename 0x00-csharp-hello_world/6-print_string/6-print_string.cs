f7366186ba5b8de68c7618277a5f48239a820e13
MarcoAnt9 <acalde27@hotmail.com>
2020-11-30 17:28:05 -0500

    Task 6: First Commit

---
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "Holberton School";
+		Console.WriteLine($"{str:s}{str:s}{str:s}\n" + str.Substring(0,9) + "\n");
+        }
+}
