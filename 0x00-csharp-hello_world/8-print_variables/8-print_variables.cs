20bc0a13226a6eedc62bae7edca70457ede481d8
MarcoAnt9 <acalde27@hotmail.com>
2020-11-30 18:02:05 -0500

    Task 8: First Commit

---
+++ 0x00-csharp-hello_world/8-print_variables/8-print_variables.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string str1 = "Street";
+        string str2 = "Mission";
+        int int1 = 972;
+        Console.WriteLine($"{int1:D} {str1:S} {str2: S}");
+    }
+}
