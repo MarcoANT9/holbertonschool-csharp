4235a4b525830e3820e78ec4016feb915691df13
MarcoAnt9 <acalde27@hotmail.com>
2020-11-30 17:32:18 -0500

    Task 6: Second Commit - Eliminated final new line

--- 0x00-csharp-hello_world/6-print_string/6-print_string.cs
+++ 0x00-csharp-hello_world/6-print_string/6-print_string.cs
@@ -5,6 +5,6 @@ class Program
 	static void Main(string[] args)
 	{
 		string str = "Holberton School";
-		Console.WriteLine($"{str:s}{str:s}{str:s}\n" + str.Substring(0,9) + "\n");
+		Console.WriteLine($"{str:s}{str:s}{str:s}\n" + str.Substring(0,9));
         }
 }
