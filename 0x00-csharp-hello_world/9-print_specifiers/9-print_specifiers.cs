b67005026a1b846f30770341bec3d5d4614d1557
MarcoAnt9 <acalde27@hotmail.com>
2020-11-30 18:31:05 -0500

    Task 9: Second Commit - Modified Write line syntax

--- 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
+++ 0x00-csharp-hello_world/9-print_specifiers/9-print_specifiers.cs
@@ -2,10 +2,11 @@
 
 class Program
 {
-	static void Main(string[] args)
-	{
-		double percent = .7553;
-		double currency = 98765.4321;
-		Console.WriteLine($"Percent: {percent:0.00%}\nCurrency: {currency:C}");
-	}
+    static void Main(string[] args)
+    {
+        double percent = .7553;
+        double currency = 98765.4321;
+        Console.WriteLine($"Percent: {percent:0.00%}");
+        Console.WriteLine($"Currency: {currency:C}");
+    }
 }
