4b66759fe38390f51a9ba8838999919231932984
MarcoAnt9 <acalde27@hotmail.com>
2020-11-30 16:22:29 -0500

    Task 1 and 2: First Commit

---
+++ 0x00-csharp-hello_world/1-build_new_project.sh.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+dotnet new console -o 1-new_project
+dotnet build ./1-new_project
---
+++ 0x00-csharp-hello_world/2-run_new_project.sh.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+dotnet new console -o 2-new_project
+dotnet build ./2-new_project
+dotnet run --project ./2-new_project
