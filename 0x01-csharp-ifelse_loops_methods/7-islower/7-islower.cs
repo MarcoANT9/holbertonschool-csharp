96afecec8da5a1c1c1d29c5a71bc9bdabef9484b
MarcoAnt9 <acalde27@hotmail.com>
2020-12-01 16:39:20 -0500

    Task 7: First Commit

---
+++ 0x01-csharp-ifelse_loops_methods/7-islower/7-islower.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _7_islower
+{
+    class Program
+    {
+        public static bool IsLower(char c)
+        {
+            if (c >= 97 && c <= 122)
+                return (true);
+            return (false);
+        }
+    }
+}
