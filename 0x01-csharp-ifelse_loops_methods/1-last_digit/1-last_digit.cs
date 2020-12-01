35519ec33415eaf9e2f246056811dec2141b9f0d
MarcoAnt9 <acalde27@hotmail.com>
2020-12-01 15:49:19 -0500

    Task 1: Second Commit - Fixed output message

--- 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -8,10 +8,10 @@ class Program
         int number = rndm.Next(-10000, 10000);
         int last = number % 10;
         if (last > 5)
-            Console.WriteLine($"The last digit of {number} is {last} ant is greater than 5");
+            Console.WriteLine($"The last digit of {number} is {last} and is greater than 5");
         else if (last == 0)
-            Console.WriteLine($"The last digit of {number} is {last} ant is 0");
+            Console.WriteLine($"The last digit of {number} is {last} and is 0");
         else
-            Console.WriteLine($"The last digit of {number} is {last} ant is less than 6 and not 0");
+            Console.WriteLine($"The last digit of {number} is {last} and is less than 6 and not 0");
     }
 }
