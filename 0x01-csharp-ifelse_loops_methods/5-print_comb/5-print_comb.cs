50b67bdd81b46adca34a776495df36aedac45628
MarcoAnt9 <acalde27@hotmail.com>
2020-12-01 16:27:39 -0500

    Task 5: First Commit

---
+++ 0x01-csharp-ifelse_loops_methods/5-print_comb/5-print_comb.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _5_print_comb
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i <= 99; i++)
+            {
+                if (i == 99)
+                    Console.Write($"{i:D2}\n");
+                else
+                    Console.Write($"{i:D02}, ");
+            }
+            
+        }
+    }
+}
