572f73cb70e94bb035a22f42f1e21e33eef792fe
MarcoAnt9 <acalde27@hotmail.com>
2020-12-01 16:34:50 -0500

    Task 6: First Commit

---
+++ 0x01-csharp-ifelse_loops_methods/6-print_comb2/6-print_comb2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _6_print_comb2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 9; i++){
+                for (int j = i + 1; j < 10; j++){
+                    if (i == 8 && j == 9)
+                        Console.Write($"{i}{j}\n");
+                    else
+                        Console.Write($"{i}{j}, ");
+                }
+            }
+        }
+    }
+}
