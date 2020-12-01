4ba9c34a9f635838c5fa2c4e62122e008cdebed4
MarcoAnt9 <acalde27@hotmail.com>
2020-12-01 16:01:50 -0500

    Task 2: First Commit

---
+++ 0x01-csharp-ifelse_loops_methods/2-print_alphabet/2-print_alphabet.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace _2_print_alphabet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (char letter = 'a'; letter <= 'z'; letter++)
+                Console.Write($"{letter}");
+        }
+    }
+}
