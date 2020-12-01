0c5644e51945786ed8c1e4b4a3db2056e29ea103
MarcoAnt9 <acalde27@hotmail.com>
2020-11-30 19:22:09 -0500

    Task 11: First Commit

---
+++ 0x00-csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+class Program
+{
+	static void Main(string[] args)
+	{
+		string str = "C# (pronounced \"See Sharp\") is a simple, modern, object-oriented, and type-safe programming language. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers.";
+		str = str.Substring(49, 15) + " " + str.Substring(81, 11) + "in" + str.Substring(101, 3);
+        Console.WriteLine(str);
+        }
+}
