c580b308423d9be31b5f3853f165e63f4db9c29e
MarcoAnt9 <acalde27@hotmail.com>
2020-12-01 16:14:12 -0500

    Task 3: First Commit

---
+++ 0x01-csharp-ifelse_loops_methods/3-print_alphabt/3-print_alphabt.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _3_print_alphabt
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int l;
+            for (l = 97; l <= 122; l++)
+            {
+                if (l == 101 || l == 113)
+                    l++;
+                char character = Convert.ToChar(l);
+                Console.Write($"{character}");
+            }
+        }
+    }
+}
