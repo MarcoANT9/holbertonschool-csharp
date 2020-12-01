07d58a8c6cd7a2c6534096a4f5743d00f059fb99
MarcoAnt9 <acalde27@hotmail.com>
2020-12-01 17:08:15 -0500

    Task 11: First Commit

---
+++ 0x01-csharp-ifelse_loops_methods/11-print_diagonal/11-print_diagonal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+
+class Line
+{
+    public static void PrintDiagonal(int length)
+    {
+        if (length <= 0)
+            Console.Write("\n");
+        else
+        {
+            for (int i = 0; i < length; i++)
+            {
+                for (int j = 0; j <= i; j++)
+                {
+                    if (j == i)
+                    {
+                        Console.Write("\\");
+                        Console.Write("\n");
+                    }
+                    else
+                        Console.Write(" ");
+                }
+            }
+        }
+    }
+}
+
