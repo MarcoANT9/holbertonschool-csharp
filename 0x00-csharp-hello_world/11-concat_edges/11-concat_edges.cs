7ee2ba1f3b8e59e072e1c241bd92bc399a83992a
MarcoAnt9 <acalde27@hotmail.com>
2020-11-30 19:27:56 -0500

    Task 11: Second Commit - Changed str definition

--- 0x00-csharp-hello_world/11-concat_edges/11-concat_edges.cs
+++ 0x00-csharp-hello_world/11-concat_edges/11-concat_edges.cs
@@ -5,7 +5,7 @@ class Program
 	static void Main(string[] args)
 	{
 		string str = "C# (pronounced \"See Sharp\") is a simple, modern, object-oriented, and type-safe programming language. C# has its roots in the C family of languages and will be immediately familiar to C, C++, Java, and JavaScript programmers.";
-		str = str.Substring(49, 15) + " " + str.Substring(81, 11) + "in" + str.Substring(101, 3);
+		str = str.Substring(49, 15) + str.Substring(79, 13) + str.Substring(119, 3) + str.Substring(0, 2);
         Console.WriteLine(str);
         }
 }
