ca053f50d8e4927a990afac9cea046722af752c3
MarcoAnt9 <acalde27@hotmail.com>
2020-12-01 15:46:31 -0500

    Task 1: First Commit

---
+++ 0x01-csharp-ifelse_loops_methods/1-last_digit/1-last_digit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10000, 10000);
+        int last = number % 10;
+        if (last > 5)
+            Console.WriteLine($"The last digit of {number} is {last} ant is greater than 5");
+        else if (last == 0)
+            Console.WriteLine($"The last digit of {number} is {last} ant is 0");
+        else
+            Console.WriteLine($"The last digit of {number} is {last} ant is less than 6 and not 0");
+    }
+}
