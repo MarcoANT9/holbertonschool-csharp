c9c33a86c342827db669fd473ad71c365e5f4edd
MarcoAnt9 <acalde27@hotmail.com>
2020-12-01 16:19:20 -0500

    Task 4: First Commit

---
+++ 0x01-csharp-ifelse_loops_methods/4-print_hexa/4-print_hexa.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _4_print_hexa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 0; number < 99; number++)
+            {
+                Console.Write($"{number:D} = 0x{number:x}\n");
+
+            }
+        }
+    }
+}
