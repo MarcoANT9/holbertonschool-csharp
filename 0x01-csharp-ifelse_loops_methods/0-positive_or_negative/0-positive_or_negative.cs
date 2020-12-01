18268cd15bbed8e2cb482b8a5b06f662a205f476
MarcoAnt9 <acalde27@hotmail.com>
2020-12-01 15:39:08 -0500

    Task 1: First Commit

---
+++ 0x01-csharp-ifelse_loops_methods/0-positive_or_negative/0-positive_or_negative.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Random rndm = new Random();
+        int number = rndm.Next(-10, 10);
+        if (number > 0)
+            Console.WriteLine($"{number} is positive");
+        else if (number < 0)
+            Console.WriteLine($"{number} is negative");
+        else
+            Console.WriteLine($"{number} is zero");
+    }
+}
