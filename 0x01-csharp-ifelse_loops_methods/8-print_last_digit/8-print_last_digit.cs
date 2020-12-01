f7f8092541ea48641414ba7ecd6c18c563b766ff
MarcoAnt9 <acalde27@hotmail.com>
2020-12-01 16:42:19 -0500

    Task 8: First Commit

---
+++ 0x01-csharp-ifelse_loops_methods/8-print_last_digit/8-print_last_digit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _8_print_last_digit
+{
+    class Number
+    {
+        public static int PrintLastDigit(int number)
+        {
+            return (number % 10);
+        }
+    }
+}
