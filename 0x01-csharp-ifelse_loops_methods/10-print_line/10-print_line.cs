a25470abf362777fc74b19f84dd454fe780b8f00
MarcoAnt9 <acalde27@hotmail.com>
2020-12-01 16:55:33 -0500

    Task 10: First Commit & gitignore update

---
+++ 0x01-csharp-ifelse_loops_methods/10-print_line/10-print_line.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Line
+{
+    public static void PrintLine(int length)
+    {
+        if (length <= 0)
+            Console.Write("\n");
+        else
+        {
+            for (int i = 0; i < length; i++)
+            {
+                Console.Write("_");
+            }
+        }
+    }
+}
