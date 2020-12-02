23539472975fe70fea56c16726f4d506ce4860eb
MarcoAnt9 <acalde27@hotmail.com>
2020-12-02 16:47:38 -0500

    Task 16: Correction
    Corrected issues in the impresion and the .csproj

--- 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -6,10 +6,23 @@ namespace _100_jagged_array
     {
         static void Main(string[] args)
         {
-            int[][] jagged_array = new int[3][];
-            jagged_array[0] = new int[] {0, 1, 2, 3};
-            jagged_array[1] = new int[] {0, 1, 2, 3, 4, 5, 6};
-            jagged_array[2] = new int[] {0, 1};
+            int[][] jagged_array = new int[3][]
+            {
+            new int[] {0, 1, 2, 3},
+            new int[] { 0, 1, 2, 3, 4, 5, 6 },
+            new int[] { 0, 1 }
+            };
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < jagged_array[i].Length; j++)
+                {
+                    Console.Write(jagged_array[i][j]);
+                    if (j < jagged_array[i].Length - 1)
+                        Console.Write(" ");
+                }
+                Console.WriteLine();
+            }
         }
     }
 }
