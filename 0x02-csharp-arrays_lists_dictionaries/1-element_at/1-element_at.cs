a203a8783d5fce0c7c5dee672f6ef47346cd8608
MarcoAnt9 <acalde27@hotmail.com>
2020-12-02 13:26:38 -0500

    Task 1: First Commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/1-element_at/1-element_at.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Array
+{
+    public static int elementAt(int[] array, int index)
+    {
+        if (index >= array.Length || index < 0)
+        {
+            Console.WriteLine("Index out of range");
+            return (-1);
+        }
+        return (array[index]);
+    }
+}
+
