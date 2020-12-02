af84e92f04a9223b704b64da482487e45a0cd1ba
MarcoAnt9 <acalde27@hotmail.com>
2020-12-02 15:23:46 -0500

    Task 11: Second commit - Added new dictionary
    New adtion to let the input unmoded

--- 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
+++ 0x02-csharp-arrays_lists_dictionaries/11-multiply_by_2/11-multiply_by_2.cs
@@ -5,10 +5,12 @@ class Dictionary
 {
     public static Dictionary<string, int> MultiplyBy2(Dictionary<string, int> myDict)
     {
-        List<string> dicKeys = new List<string>(myDict.Keys);
-        foreach (string element in dicKeys)
-            myDict[element] = myDict[element] * 2;
-        return (myDict);
+        // Creates a new dictionary to copy the info from the first and not modify the first.
+        Dictionary<string, int> myNewDict = new Dictionary<string, int>();
+        foreach (string element in myDict.Keys)
+            myNewDict[element] = myDict[element] * 2;
+
+        return (myNewDict);
     }
 }
 
