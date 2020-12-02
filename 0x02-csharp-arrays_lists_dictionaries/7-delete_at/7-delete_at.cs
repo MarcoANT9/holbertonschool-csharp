18fe0f98278a062a59752e8905a3f8ea9b640111
MarcoAnt9 <acalde27@hotmail.com>
2020-12-02 14:30:03 -0500

    Task 7: First Commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DeleteAt(List<int> myList, int index)
+    {
+        if (index > myList.Count || index < 0)
+        {
+            Console.WriteLine("Index is out of range");
+            return (myList);
+        }
+        int count = 0;
+        for (int i = 0; i < index; i++)
+            count++;
+        myList.Remove(myList[count]);
+        return(myList);
+    }
+}
+
