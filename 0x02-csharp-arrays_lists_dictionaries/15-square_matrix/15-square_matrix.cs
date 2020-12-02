fce3c8d0e95013b41641019d72b66b5c47009413
MarcoAnt9 <acalde27@hotmail.com>
2020-12-02 16:34:53 -0500

    Task 15: First Commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/15-square_matrix/15-square_matrix.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Matrix
+{
+    public static int[,] Square(int[,] myMatrix)
+    {
+        int dim = myMatrix.Rank; // Number of dimensions in the matrix
+        int[] dimensions = new int[2];
+
+        for (int dimension = 1; dimension <= dim; dimension++)
+            dimensions[dimension - 1] = myMatrix.GetUpperBound(dimension - 1) + 1;
+
+        int hei = dimensions[0];
+        int wid = dimensions[1];
+
+        int[, ] new_array = new int[hei, wid];
+
+        for (int i = 0; i < hei; i++)
+        {
+            for (int j = 0; j < wid; j++)
+                new_array[i, j] = myMatrix[i, j] * myMatrix[i, j];
+        }
+        return (new_array);
+    }
+}
