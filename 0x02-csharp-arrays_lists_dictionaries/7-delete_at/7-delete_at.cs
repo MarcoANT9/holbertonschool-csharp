b831d552edae4d415390eedb9572da6ad7723c02
MarcoAnt9 <acalde27@hotmail.com>
2020-12-02 14:35:46 -0500

    Task 7: Second Commit - Updated out of range condition

--- 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
+++ 0x02-csharp-arrays_lists_dictionaries/7-delete_at/7-delete_at.cs
@@ -5,7 +5,7 @@ class List
 {
     public static List<int> DeleteAt(List<int> myList, int index)
     {
-        if (index > myList.Count || index < 0)
+        if (index >= myList.Count || index < 0)
         {
             Console.WriteLine("Index is out of range");
             return (myList);
