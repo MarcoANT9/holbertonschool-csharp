cb656e423e074cf16fc573dd609ad74dd410ffc6
MarcoAnt9 <acalde27@hotmail.com>
2020-12-02 14:16:05 -0500

    Task 6: First Commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/6-divisible_by_2/6-divisible_by_2.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<bool> DivisibleBy2(List<int> myList)
+    {
+        List<bool> true_false = new List<bool>();
+        foreach (int element in myList)
+        {
+            if (element % 2 == 0)
+                true_false.Add(true);
+            else
+                true_false.Add(false);
+        }
+        return (true_false);
+    }
+}
+
