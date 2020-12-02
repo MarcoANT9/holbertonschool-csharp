0cc6d29a1579e8b4c6520dcde2ac6cc781502bd7
MarcoAnt9 <acalde27@hotmail.com>
2020-12-02 16:00:21 -0500

    Task 13: First Commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/13-best_score/13-best_score.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static string BestScore(Dictionary<string, int> myList)
+    {
+        if (myList.Count == 0)
+            return ("None");
+
+        int max_value = -1;
+        string name = "";
+        foreach (string element in myList.Keys)
+        {
+            if (myList[element] > max_value)
+            {
+                max_value = myList[element];
+                name = element;
+            }
+        }
+        if (name == "")
+            return ("None");
+        return (name);
+    }
+}
+
