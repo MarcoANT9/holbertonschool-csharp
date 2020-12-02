963d3ca70cf0194803bcf4e7bb0f008029186e80
MarcoAnt9 <acalde27@hotmail.com>
2020-12-02 13:41:42 -0500

    Task 3: First Commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/3-print_array_revers/3-print_array_revers.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Array
+{
+    public static void Reverse(int[] array)
+    {
+        if (array != null)
+        {
+            for (int i = array.Length - 1; i >= 0; i--)
+            {
+                if (i > 0)
+                    Console.Write(array[i] + " ");
+                else
+                    Console.Write(array[i]);
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
