ddfad0eca814cc0e2de000ced11700248b89dbcd
MarcoAnt9 <acalde27@hotmail.com>
2020-12-02 15:45:11 -0500

    Task 12: First Commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/12-print_sorted_dictionary/12-print_sorted_dictionary.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static void PrintSorted(Dictionary<string, string> myDict)
+    {
+        // Create a new list to store the sorted keys in the dict
+        List<string> sorted_dict = new List<string>(myDict.Keys);
+        // Sort the keys
+        sorted_dict.Sort();
+        // Print the sorted keys : values
+        foreach (string element in sorted_dict)
+            Console.WriteLine(element + ": " + myDict[element]);
+    }
+}
+
