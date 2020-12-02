2181377e1e4a21b0b18d5101ebef3f86ac7061e6
MarcoAnt9 <acalde27@hotmail.com>
2020-12-02 13:54:35 -0500

    Task 4: First Commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/4-print_list/4-print_list.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static List<int> CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+        List<int> numbers = new List<int>();
+
+        for (int i = 0; i < size; i++)
+        {
+            numbers.Add(i);
+            if (i < size - 1)
+                Console.Write(i + " ");
+            else
+                Console.Write(i);
+        }
+        Console.WriteLine();
+        return (numbers);
+    }
+}
+
