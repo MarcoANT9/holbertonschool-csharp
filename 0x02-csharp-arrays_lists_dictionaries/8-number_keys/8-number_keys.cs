50baa8a79e1d5b501a1691610e86c34bf6064576
MarcoAnt9 <acalde27@hotmail.com>
2020-12-02 14:43:48 -0500

    Task 8: First Commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/8-number_keys/8-number_keys.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Dictionary
+{
+    public static int NumberOfKeys(Dictionary<string, string> myDict)
+    {
+        int count = 0;
+        foreach (string element in myDict.Keys)
+            count++;
+        return (count);
+    }
+}
+
