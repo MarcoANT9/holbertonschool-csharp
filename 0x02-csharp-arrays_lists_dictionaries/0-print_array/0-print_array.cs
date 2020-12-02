d38ec83ec82a9f3de8586e9ed91453f3090f0567
MarcoAnt9 <acalde27@hotmail.com>
2020-12-02 13:18:17 -0500

    Readme & Task 0: First Commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/0-print_array/0-print_array.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Array
+{
+    public static int[] CreatePrint(int size)
+    {
+        if (size < 0)
+        {
+            Console.WriteLine("Size cannot be negative");
+            return (null);
+        }
+        if (size == 0)
+            Console.WriteLine();
+        int[] num_array = new int[size];
+        for (int i = 0; i < num_array.Length; i++)
+        {
+            num_array[i] = i;
+            if (i < num_array.Length - 1)
+                Console.Write(num_array[i] + " ");
+            else
+                Console.Write(num_array[i] + "\n");
+        }
+        return (num_array);
+
+    }
+}
---
+++ 0x02-csharp-arrays_lists_dictionaries/README.md
@@ -0,0 +1,1 @@
+0x02. C# - Data Structures: Arrays, Lists, Dictionaries
