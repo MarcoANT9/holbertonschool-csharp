4c17dd4325ed894c6c1346b5ecf77f3b5063e18d
MarcoAnt9 <acalde27@hotmail.com>
2020-12-02 16:14:22 -0500

    Task 14: First Commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/14-rectangular_array/14-rectangular_array.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Array
+{
+    static void Main(string[] args)
+    {
+        int i = 5;
+        int j = 5;
+
+        int[,] myArray = new int[i, j];
+
+        myArray[2, 2] = 1;
+        for (int w = 0; w < i; w++)
+        {
+            for (int h = 0; h < j; h++)
+            {
+                Console.Write(myArray[w, h]);
+                if (h < j - 1)
+                    Console.Write(" ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
+
