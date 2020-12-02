b9244e183b56e337834c1784291cc739ab5aaa0f
MarcoAnt9 <acalde27@hotmail.com>
2020-12-02 16:40:00 -0500

    Task 16: First Commit

---
+++ 0x02-csharp-arrays_lists_dictionaries/100-jagged_array/100-jagged_array.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _100_jagged_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[][] jagged_array = new int[3][];
+            jagged_array[0] = new int[] {0, 1, 2, 3};
+            jagged_array[1] = new int[] {0, 1, 2, 3, 4, 5, 6};
+            jagged_array[2] = new int[] {0, 1};
+        }
+    }
+}
