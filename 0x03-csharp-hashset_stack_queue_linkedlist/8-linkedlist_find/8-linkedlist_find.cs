e9b41c44482babcee264ed9bf8994a04ae6706c0
MarcoAnt9 <acalde27@hotmail.com>
2020-12-03 16:23:52 -0500

    Task 8: First Commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/8-linkedlist_find/8-linkedlist_find.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int FindNode(LinkedList<int> myLList, int value)
+    {
+        int index = 0;
+        foreach (int element in myLList)
+        {
+            if (element == value)
+                return (index);
+            index++;
+        }
+        return (-1);
+    }
+}
+
