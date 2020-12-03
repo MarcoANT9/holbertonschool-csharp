32180544b57dd58123e06c6797964a8464ece57c
MarcoAnt9 <acalde27@hotmail.com>
2020-12-03 14:33:59 -0500

    Task 1: First Commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/1-common_elements/1-common_elements.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> CommonElements(List<int> list1, List<int> list2)
+    {
+        List<int> listSorted = new List<int>();
+        foreach (int element in list1)
+        {
+            foreach (int number in list2)
+            {
+                if (element == number)
+                {
+                    listSorted.Add(element);
+                    break;
+                }
+            }
+        }
+        listSorted.Sort();
+        return (listSorted);
+    }
+}
+
