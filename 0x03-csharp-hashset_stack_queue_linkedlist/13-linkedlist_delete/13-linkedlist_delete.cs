8d5072a1d00355ac9ea330c02b11294b95e2081b
MarcoAnt9 <acalde27@hotmail.com>
2020-12-03 18:02:13 -0500

    Task 13: First Commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static void Delete(LinkedList<int> myLList, int index)
+    {
+        LinkedListNode<int> currentNode = myLList.First;
+        if (index == myLList.Count)
+            myLList.RemoveLast();
+        else
+        {
+            for (int i = 0; i <= index && currentNode != null; i++)
+            {
+                if (i == index)
+                {
+                    myLList.Remove(currentNode);
+                    break;
+                }
+                currentNode = currentNode.Next;
+            }
+        }
+    }
+}
+
