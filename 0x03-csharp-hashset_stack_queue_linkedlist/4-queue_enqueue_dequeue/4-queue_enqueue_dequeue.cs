d837c5bdfbe2d3f2d51bae19582d84e7c05ec2e2
MarcoAnt9 <acalde27@hotmail.com>
2020-12-03 16:01:04 -0500

    Task 4: First Commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/4-queue_enqueue_dequeue/4-queue_enqueue_dequeue.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyQueue
+{
+    public static Queue<string> Info(Queue<string> aQueue, string newItem, string search)
+    {
+        Console.WriteLine($"Number of items: {aQueue.Count}");
+
+        if (aQueue.Count == 0)
+            Console.WriteLine("Queue is empty");
+        else
+            Console.WriteLine($"First item: {aQueue.Peek()}");
+
+        aQueue.Enqueue(newItem);
+
+        Console.WriteLine($"Queue contains \"{search}\": {aQueue.Contains(search)}");
+
+        if (aQueue.Contains(search))
+        {
+            while(aQueue.Contains(search))
+                aQueue.Dequeue();
+        }
+
+        return(aQueue);
+    }
+}
+
