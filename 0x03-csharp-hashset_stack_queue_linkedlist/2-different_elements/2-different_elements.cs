2d947b9deb2090449982708c4fd1ca9ad2bf74f7
MarcoAnt9 <acalde27@hotmail.com>
2020-12-03 14:43:34 -0500

    Task 2: First Commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/2-different_elements/2-different_elements.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static List<int> DifferentElements(List<int> list1, List<int> list2)
+    {
+        List<int> listSorted = new List<int>();
+        foreach (int element in list1)
+        {
+            int same = 0;
+            foreach (int number in list2)
+            {
+                if (element == number)
+                {
+                    same = 1;
+                    break;
+                }
+            }
+            if (same == 0)
+                listSorted.Add(element);
+        }
+        foreach (int number in list2)
+        {
+            int same = 0;
+            foreach (int element in list1)
+            {
+                if (number == element)
+                {
+                    same = 1;
+                    break;
+                }
+            }
+            if (same == 0)
+                listSorted.Add(number);
+        }
+        listSorted.Sort();
+        return (listSorted);
+    }
+}
