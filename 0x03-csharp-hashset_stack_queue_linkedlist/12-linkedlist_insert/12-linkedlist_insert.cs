95f32e9062e475ccb272d17348ba682c2319911d
MarcoAnt9 <acalde27@hotmail.com>
2020-12-03 17:31:35 -0500

    Task 12: First Commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
+    {
+        foreach (int element in myLList)
+        {
+            if (element > n)
+                {
+                    myLList.AddBefore(myLList.Find(element), n);
+                    break;
+                }
+        }
+        
+        return (myLList.Find(n));
+    }
+}
+
