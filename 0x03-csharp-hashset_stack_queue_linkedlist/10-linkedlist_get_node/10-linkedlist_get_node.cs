bac928999f6ea9d739b5e9ef12b3d53b15fc71ad
MarcoAnt9 <acalde27@hotmail.com>
2020-12-03 16:49:04 -0500

    Task 10: First Commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/10-linkedlist_get_node/10-linkedlist_get_node.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int GetNode(LinkedList<int> myLList, int n)
+    {
+        if (myLList.Count < n)
+            return (0);
+        int index = 0;
+        foreach (int element in myLList)
+        {
+            if (index == n)
+                return (element);
+            index++;
+        }
+        return (0);
+    }
+}
+
