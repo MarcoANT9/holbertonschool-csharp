ba65d9b9d943dbd548d700b7c77257a9d887c98e
MarcoAnt9 <acalde27@hotmail.com>
2020-12-03 18:15:12 -0500

    Task 13: Second Commit
    Modified edge cases and internal changes logic

--- 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/13-linkedlist_delete/13-linkedlist_delete.cs
@@ -6,18 +6,26 @@ class LList
     public static void Delete(LinkedList<int> myLList, int index)
     {
         LinkedListNode<int> currentNode = myLList.First;
-        if (index == myLList.Count)
+
+        if (myLList != null && index == myLList.Count - 1)
             myLList.RemoveLast();
+
+        else if (myLList != null && index == 0)
+            myLList.RemoveFirst();
+
         else
         {
-            for (int i = 0; i <= index && currentNode != null; i++)
+            if (myLList != null && index >= 0)
             {
-                if (i == index)
+                for (int i = 0; i < index && currentNode != null; i++)
                 {
-                    myLList.Remove(currentNode);
-                    break;
+                    if (i == index)
+                    {
+                        myLList.Remove(currentNode);
+                        break;
+                    }
+                    currentNode = currentNode.Next;
                 }
-                currentNode = currentNode.Next;
             }
         }
     }
