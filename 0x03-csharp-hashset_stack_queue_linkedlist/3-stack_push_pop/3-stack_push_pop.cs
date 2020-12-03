cddb63f38b60f70c65f2a453c97e280b7b8022da
MarcoAnt9 <acalde27@hotmail.com>
2020-12-03 15:00:10 -0500

    Task 3: First Commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/3-stack_push_pop/3-stack_push_pop.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+
+class MyStack
+{
+    public static Stack<string> Info(Stack<string> aStack, string newItem, string search)
+    {
+        int counter = aStack.Count;
+        Console.WriteLine($"Number of items: {counter}");
+
+        if (counter == 0)
+            Console.WriteLine("Stack is empty");
+        else
+            Console.WriteLine($"Top item: {aStack.Peek()}");
+
+        Console.Write($"Stack contains: {search}: ");
+        int exist = 0;
+
+        foreach (string element in aStack)
+        {
+            if (element == search)
+            {
+                exist = 1;
+                break;
+            }
+        }
+        if (exist == 0)
+            Console.WriteLine("False");
+        else
+        {
+            Console.WriteLine("True");
+            int i = 1;
+            while (i != 0)
+            {
+                if (aStack.Peek() == search)
+                {
+                    aStack.Pop();
+                    break;
+                }
+                else
+                    aStack.Pop();
+            }
+        }
+        aStack.Push(newItem);
+        return (aStack);
+    }
+}
+
