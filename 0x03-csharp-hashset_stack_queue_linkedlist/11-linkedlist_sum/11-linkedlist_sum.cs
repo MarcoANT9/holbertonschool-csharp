e0c0c473bbfc9fe4fbaa6ce5300fd296ca3452c6
MarcoAnt9 <acalde27@hotmail.com>
2020-12-03 16:54:34 -0500

    Task 11: First Commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/11-linkedlist_sum/11-linkedlist_sum.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Sum(LinkedList<int> myLList)
+    {
+        int suma = 0;
+
+        foreach (int element in myLList)
+            suma += element;
+
+        return (suma);
+    }
+}
+
