afa0bf1b5b670cb660c4f2e7be9b1eb0a7b6d0af
MarcoAnt9 <acalde27@hotmail.com>
2020-12-03 17:46:29 -0500

    Task 12: Second Commit - Updated edge cases.

--- 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
+++ 0x03-csharp-hashset_stack_queue_linkedlist/12-linkedlist_insert/12-linkedlist_insert.cs
@@ -5,15 +5,24 @@ class LList
 {
     public static LinkedListNode<int> Insert(LinkedList<int> myLList, int n)
     {
-        foreach (int element in myLList)
+
+        if (n < myLList.First.Value)
+            myLList.AddFirst(n);
+
+        else if (n > myLList.Last.Value)
+            myLList.AddLast(n);
+
+        else
         {
-            if (element > n)
+            foreach (int element in myLList)
+            {
+                if (element > n)
                 {
                     myLList.AddBefore(myLList.Find(element), n);
                     break;
                 }
+            }
         }
-        
         return (myLList.Find(n));
     }
 }
