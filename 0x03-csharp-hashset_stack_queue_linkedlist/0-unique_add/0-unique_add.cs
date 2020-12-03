0ae8e2b1ed6bd497ff6d9f17522cead204faeb00
MarcoAnt9 <acalde27@hotmail.com>
2020-12-03 14:25:12 -0500

    Task 0: First Commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/0-unique_add/0-unique_add.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+class List
+{
+    public static int Sum(List<int> myList)
+    {
+        var uniqList = new HashSet<int>(myList);
+        int suma = 0;
+        foreach(int element in uniqList)
+            suma += element;
+        return(suma);
+    }
+}
+
