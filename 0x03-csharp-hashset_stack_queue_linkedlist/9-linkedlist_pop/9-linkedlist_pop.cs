08d8093dacc83899d31e0d62fe389b2ca3b9a602
MarcoAnt9 <acalde27@hotmail.com>
2020-12-03 16:31:25 -0500

    Task 9: First Commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/9-linkedlist_pop/9-linkedlist_pop.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static int Pop(LinkedList<int> myLList)
+    {
+        if (myLList.Count == 0)
+            return (0);
+        
+        int value = myLList.First.Value;
+        myLList.RemoveFirst();
+        return (value);
+    }
+}
+
