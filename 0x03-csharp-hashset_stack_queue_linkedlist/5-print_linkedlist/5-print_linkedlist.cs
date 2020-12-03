ad465d363c986c3415cd907e5d97689d1f9b3f14
MarcoAnt9 <acalde27@hotmail.com>
2020-12-03 16:07:07 -0500

    Task 5: First Commit

---
+++ 0x03-csharp-hashset_stack_queue_linkedlist/5-print_linkedlist/5-print_linkedlist.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+class LList
+{
+    public static LinkedList<int> CreatePrint(int size)
+    {
+        LinkedList<int> myLList = new LinkedList<int>();
+
+        if (size < 0)
+            return (myLList);
+
+        for (int i= 0; i < size; i++)
+         {
+             Console.WriteLine(i);
+            myLList.AddLast(i);
+         }   
+        
+        return(myLList);
+
+    }
+}
+
