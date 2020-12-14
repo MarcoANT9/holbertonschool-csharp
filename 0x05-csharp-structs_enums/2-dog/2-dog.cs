eba0a24e197d50b6c2e8e98e748a3ffd5c5ad350
MarcoAnt9 <acalde27@hotmail.com>
2020-12-14 15:46:09 -0500

    Task 2: First Commit

---
+++ 0x05-csharp-structs_enums/2-dog/2-dog.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent,
+}
+
+public struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string name, float age, string owner, Rating rating)
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+}
