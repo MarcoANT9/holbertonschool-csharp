ce05306514e38508ead14f4775a022376c7cf902
MarcoAnt9 <acalde27@hotmail.com>
2020-12-14 15:32:42 -0500

    Task 1: First Commit

---
+++ 0x05-csharp-structs_enums/1-dog/1-dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent,
+}
+
+public struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+}
