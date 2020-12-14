bc3440ccde9bfcfecb55fc9530cc1b3973c82c71
MarcoAnt9 <acalde27@hotmail.com>
2020-12-14 14:29:19 -0500

    Task 1: First Commit & gitignore fix

---
+++ 0x04-csharp-exceptions/1-divide_print/1-divide_print.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Int
+{
+    public static void divide(int a, int b)
+    {
+        try
+        {
+            Console.WriteLine($"{a} / {b} = {a / b}");
+        }
+        catch (DivideByZeroException)
+        {
+            Console.WriteLine("Cannot divide by zero");
+            Console.WriteLine($"{a} / {b} = 0");
+        }
+    }
+}
