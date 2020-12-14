9afcb1f43869406ba3c8d4b0b4ce228f6d0909a1
MarcoAnt9 <acalde27@hotmail.com>
2020-12-14 15:54:43 -0500

    Task 3: First Commit

---
+++ 0x05-csharp-structs_enums/3-dog/3-dog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public enum Rating
+{
+    Good,
+    Great,
+    Excellent,
+}
+
+public struct Dog
+{
+    public string name;
+    public float age;
+    public string owner;
+    public Rating rating;
+
+    public Dog(string name, float age, string owner, Rating rating)
+    {
+        this.name = name;
+        this.age = age;
+        this.owner = owner;
+        this.rating = rating;
+    }
+
+    public override string ToString()
+    {
+        string ret_str = $"Dog name: {this.name}\nAge: {this.age}\nOwner: {this.owner}\nRating: {this.rating}";
+        return ret_str;
+    }
+}
