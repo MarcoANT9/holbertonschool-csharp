d7e75cd9102c457a753c2c61d354650a942482eb
MarcoAnt9 <acalde27@hotmail.com>
2020-12-14 14:21:36 -0500

    Task 0: First Commit

---
+++ 0x04-csharp-exceptions/0-safe_list_print/0-safe_list_print.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+class List
+{
+    public static int SafePrint(List<int> myList, int n)
+    {
+        int i = 0;
+        try
+        {
+            while (i < n)    
+            {
+                Console.WriteLine(myList[i]);
+                i++;
+            }
+            return (i);
+        }
+        
+        catch (System.ArgumentOutOfRangeException)
+        {
+            return (i);
+        }
+    }
+}
