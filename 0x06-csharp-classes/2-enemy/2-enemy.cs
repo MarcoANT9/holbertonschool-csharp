cecc9cd0327972db8f3abb623f93a210cd9b26de
MarcoAnt9 <acalde27@hotmail.com>
2020-12-15 16:24:03 -0500

    Task 2: First Commit

---
+++ 0x06-csharp-classes/2-enemy/2-enemy.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Enemies
+{
+    /// <summary>This class represents a zombie enemy. </summary>
+    public class Zombie
+    {
+
+        /// <values>This is the value that represents the hit points of the enemy. </values>
+        public int health;
+
+        /// <summary>This is the constructor of the class. </summary>
+        public Zombie()
+        {
+            health = 0;
+        }
+        ///<summary>
+        ///This is another constructor for the class, it verifies that the health is >= 0.
+        ///</summary>
+        public Zombie(int value)
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException ("Health must be greater than or equal to 0");
+            }
+            health = value;
+        }
+    }
+}
