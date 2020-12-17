4798d49bf222bc6e4024edb8f746df34e4ed9268
MarcoAnt9 <acalde27@hotmail.com>
2020-12-17 18:25:33 -0500

    Task 0: Updated tests

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -11,17 +11,16 @@ namespace MyMath.Tests
         {
         }
 
-        [Test]
         [TestCase(1, 1)]
         [TestCase(1, -1)]
         [TestCase(-2, -4)]
         [TestCase(2, 0)]
         [TestCase(0, -5)]
         [TestCase(0, 0)]
-        public void Test1_A1_B1_Returns_2(int a, int b)
+        public void Test_Returns_A_Plus_B(int a, int b)
         {
             int result = Operations.Add(a, b);
-            Assert.AreEqual(result, a + b);
+            Assert.AreEqual(a + b, result);
         }
     }
 }
