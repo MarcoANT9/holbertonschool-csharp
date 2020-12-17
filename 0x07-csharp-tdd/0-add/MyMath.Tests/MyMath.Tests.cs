1abda3dc3b5d6b77f0708e60a2f5179e718f6393
MarcoAnt9 <acalde27@hotmail.com>
2020-12-17 16:57:55 -0500

    Task 0: First Commit

---
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -0,0 +1,26 @@
+using NUnit.Framework;
+using MyMath;
+
+namespace MyMath.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        [TestCase(1, 1)]
+        [TestCase(1, -1)]
+        [TestCase(-2, -4)]
+        [TestCase(2, 0)]
+        [TestCase(0, 5)]
+        public void Test1_A1_B1_Returns_2(int a, int b)
+        {
+            int result = Operations.Add(a, b);
+            Assert.AreEqual(result, a + b);
+        }
+    }
+}
---
+++ 0x07-csharp-tdd/0-add/MyMath/MyMath.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace MyMath
+{
+    /// <summary>This class is to store operations.</summary>
+    public class Operations
+    {
+        /// <summary>This is the addition operation, returns the sum of the inputs.</summary>
+        public static int Add(int a, int b)
+        {
+            return (a + b);
+        }
+        
+    }
+}
