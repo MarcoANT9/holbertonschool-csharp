a0eba0e067f8e901fe10a3978037d4262b94d164
MarcoAnt9 <acalde27@hotmail.com>
2020-12-17 17:07:42 -0500

    Updated Tests: Zero + Zero

--- 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
+++ 0x07-csharp-tdd/0-add/MyMath.Tests/MyMath.Tests.cs
@@ -17,6 +17,7 @@ namespace MyMath.Tests
         [TestCase(-2, -4)]
         [TestCase(2, 0)]
         [TestCase(0, -5)]
+        [TestCase(0, 0)]
         public void Test1_A1_B1_Returns_2(int a, int b)
         {
             int result = Operations.Add(a, b);
