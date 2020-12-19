fa145b56d2a34d44e366fff44f38863be71b7ed0
MarcoAnt9 <acalde27@hotmail.com>
2020-12-19 16:05:25 -0500

    Task 0: added comments

--- 0x08. C# - Inheritance/0-is/0-is.cs
+++ 0x08. C# - Inheritance/0-is/0-is.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 
+/// <summary>This is the class Obj.</summary>
 class Obj
 {
+    /// <summary>This method determines if an object is an instance of the class int.</summary>
     public static bool IsOfTypeInt(object obj)
     {
         if (obj is int)
