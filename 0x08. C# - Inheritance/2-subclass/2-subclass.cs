6d65db556a6818d2c9ceac6216673b68142937a0
MarcoAnt9 <acalde27@hotmail.com>
2020-12-19 16:24:25 -0500

    Task 2: First Commit

---
+++ 0x08. C# - Inheritance/2-subclass/2-subclass.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+/// <summary>This is the class Obj.</summary>
+class Obj
+{
+    /// <summary>This method determines if an object is an instance of a class
+    /// that inherits from the specified class; the object must be a subclass.</summary>
+    public static bool IsOnlyASubclass(Type derivedType, Type baseType)
+    {
+        if (derivedType.IsSubclassOf(baseType))
+            return true;
+        return false;
+    }
+}
+
