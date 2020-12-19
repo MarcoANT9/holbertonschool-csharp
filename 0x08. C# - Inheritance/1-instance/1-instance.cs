e02fca71aaa2156d19bf96839b9b04d9703285e3
MarcoAnt9 <acalde27@hotmail.com>
2020-12-19 16:15:47 -0500

    Task 1: First Commit

---
+++ 0x08. C# - Inheritance/1-instance/1-instance.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+/// <summary>This is the class Obj.</summary>
+class Obj
+{
+    /// <summary>This method determines if an object is an instance of the class Array.</summary>
+    public static bool IsInstanceOfArray(object obj)
+    {
+        if (obj is Array)
+            return true;
+        return false;
+    }
+}
