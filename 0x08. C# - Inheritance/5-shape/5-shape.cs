d1961ace40d3c32bad9b534e9c0a43ce800092ff
MarcoAnt9 <acalde27@hotmail.com>
2020-12-19 17:32:26 -0500

    Task 5 & 6: First Commit

---
+++ 0x08. C# - Inheritance/5-shape/5-shape.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+/// <summary>This is a base class for shapes.</summary>
+class Shape
+{
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+
---
+++ 0x08. C# - Inheritance/6-shape/6-shape.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+/// <summary>This is a base class for shapes.</summary>
+class Shape
+{
+    /// <summary>Calculates the area of a shape.</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+/// <summary>This is a class Rectangle, it inherits from the Shape Class.</summary>
+class Rectangle : Shape
+{
+    private int width;
+    private int height;
+    /// <summary>Property Width, must be positive.</summary>
+    public int Width
+    {
+        get { return width; }
+        set
+        {
+            if (width < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            else
+                this.width = value;
+        }
+    }
+    /// <summary>Property Height, must be positive.</summary>
+    public int Height
+    {
+        get { return height; }
+        set
+        {
+            if (height < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            else
+                this.height = value;
+        }
+    }
+}
