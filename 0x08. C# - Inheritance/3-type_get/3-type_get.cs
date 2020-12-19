28d587d314855a7dc25fdd77a0f0764015090606
MarcoAnt9 <acalde27@hotmail.com>
2020-12-19 16:58:52 -0500

    Task 3: First Commit

---
+++ 0x08. C# - Inheritance/3-type_get/3-type_get.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Reflection;
+
+/// <summary>This is the class Obj.</summary>
+class Obj
+{
+    /// <summary>This method prints the names of available properties and methods
+    /// of an object.</summary>
+    public static void Print(object myObj)
+    {
+        Type t = myObj.GetType();
+        TypeInfo myType = t.GetTypeInfo();
+        PropertyInfo[] props = t.GetProperties();
+        MethodInfo[] methods = t.GetMethods();
+        
+        Console.WriteLine(myType.Name + " Properties:");
+        foreach (var element in props)
+            Console.WriteLine(element.Name);
+
+        Console.WriteLine(myType.Name + " Methods:");
+        foreach (var element in methods)
+            Console.WriteLine(element.Name);
+
+    }
+}
