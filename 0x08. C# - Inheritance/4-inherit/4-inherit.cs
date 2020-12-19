51cf4939abeb4020e7584aca2c38def7b853df70
MarcoAnt9 <acalde27@hotmail.com>
2020-12-19 17:05:57 -0500

    Task 4: First Commit

---
+++ 0x08. C# - Inheritance/4-inherit/4-inherit.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+/// <summary>This is the base class.</summary>
+public class Animal
+{
+    // Empty (<._.<)
+}
+/// <summary>This is the derived class Dog.</summary>
+public class Dog : Animal
+{
+    // Also Emtpy (>._.>)
+}
