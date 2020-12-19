3451a098868037c3d76059947a6cdf94f1ac91d7
MarcoAnt9 <acalde27@hotmail.com>
2020-12-19 17:38:44 -0500

    Task 7: First Commit

---
+++ 0x08. C# - Inheritance/7-shape/7-shape.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+/// <summary>This is a base class for shapes.</summary>
+class Shape
+{
+    /// <summary>Calculates the area of a shape.</summary>
+    public virtual int Area()
+    {
+        throw new NotImplementedException("Area() is not implemented");
+    }
+}
+/// <summary>This is a class Rectangle, it inherits from the Shape Class.</summary>
+class Rectangle : Shape
+{
+    //=============================================================================================
+    // ==== Constructors ==========================================================================
+    //=============================================================================================
+    private int width;
+    private int height;
+    /// <summary>Property Width, must be positive.</summary>
+    public int Width
+    {
+        get { return width; }
+        set
+        {
+            if (width < 0)
+                throw new ArgumentException("Width must be greater than or equal to 0");
+            else
+                this.width = value;
+        }
+    }
+    /// <summary>Property Height, must be positive.</summary>
+    public int Height
+    {
+        get { return height; }
+        set
+        {
+            if (height < 0)
+                throw new ArgumentException("Height must be greater than or equal to 0");
+            else
+                this.height = value;
+        }
+    }
+    //=============================================================================================
+    // ==== Methods ===============================================================================
+    //=============================================================================================
+
+    ///<summary>This method calculates the area of the Rectangle.</summary>
+    public new int Area()
+    {
+        return (width * height);
+    }
+
+    ///<summary>This method overrides the .ToString method.</summary>
+    public override string ToString()
+    {
+        return ($"[Rectangle] {width} / {height}");
+    }
+}
