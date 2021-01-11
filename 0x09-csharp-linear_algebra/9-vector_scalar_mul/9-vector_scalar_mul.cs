ccb2f23c0a7abdaf67e34190d259ae621a42d16c
MarcoAnt9 <acalde27@hotmail.com>
2021-01-11 17:18:11 -0500

    Task 9: First Commit

---
+++ 0x09-csharp-linear_algebra/9-vector_scalar_mul/9-vector_scalar_mul.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double[] Multiply(double[] vector, double scalar)
+    {
+        double[] product = { -1, -1 };
+        if (vector.Length < 2 || vector.Length > 3)
+            return product;
+
+        for (int i = 0; i < vector.Length; i++)
+            vector[i] *= scalar;
+
+        return (vector);
+    }
+}
+
