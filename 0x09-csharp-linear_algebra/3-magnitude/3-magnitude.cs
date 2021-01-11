23d59990753abcb36d7dbcf56642914f2f962e35
MarcoAnt9 <acalde27@hotmail.com>
2021-01-11 16:14:57 -0500

    Tasks 2 & 3: First Commit

---
+++ 0x09-csharp-linear_algebra/3-magnitude/3-magnitude.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class VectorMath
+{
+    public static double Magnitude(double[] vector)
+    {
+        if (vector.Length < 2 || vector.Length > 3)
+            return -1;
+        double mag = 0;
+        foreach (double element in vector)
+        {
+            mag += (element * element);
+        }
+        mag = Math.Round(Math.Sqrt(mag), 2);
+        return mag;
+    }
+}
+
