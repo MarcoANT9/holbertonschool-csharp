dbae3b1d8dfa2d6bbe1d339735d39ff32008827f
MarcoAnt9 <acalde27@hotmail.com>
2021-01-18 16:37:49 -0500

    Taks 12: update

--- 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -3,8 +3,9 @@
 /// <summary>This is the class object.</summary>
 class VectorMath
 {
-    /// <summary>This method gets either a 2D or 3D vector and returns its magnitude,
-    ///if the vector is not 2D or 3D it returns -1.</summary>
+    /// <summary>This method gets either a 2D or 3D vector and returns dot product,
+    ///if the vector is not 2D or 3D, or the size of the vectors is different it
+    ///returns -1.</summary>
     public static double DotProduct(double[] vector1, double[] vector2)
     {
         if (
