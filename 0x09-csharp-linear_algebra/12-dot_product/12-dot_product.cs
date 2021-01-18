68783d99c0c86d3205f511a2cf6f58b519ed8e01
MarcoAnt9 <acalde27@hotmail.com>
2021-01-18 16:33:50 -0500

    Update 12-dot_product.cs

--- 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
+++ 0x09-csharp-linear_algebra/12-dot_product/12-dot_product.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 
+/// <summary>This is the class object.</summary>
 class VectorMath
 {
+    /// <summary>This method gets either a 2D or 3D vector and returns its magnitude,
+    ///if the vector is not 2D or 3D it returns -1.</summary>
     public static double DotProduct(double[] vector1, double[] vector2)
     {
-        if (vector1.Length > 3 || vector2 > 3)
+        if (
+            (vector1.Length > 3 || vector2.Length > 3) ||
+            (vector1.Length < 2 || vector2.Length < 2) ||
+            (vector1.Length != vector2.Length)
+        )
+            return (-1);
+        
+        double sum = 0;
+
+        for (int i = 0; i < vector1.Length; i++)
+            sum += vector1[i] * vector2[i];
+
+        return (sum);
     }
 }
 
