540e8c8a48012e8d6f7fdadaa56a7365ed5300b2
MarcoAnt9 <acalde27@hotmail.com>
2021-01-20 17:47:20 -0500

    Update 28-determinant.cs

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -11,7 +11,7 @@ class MatrixMath
             (matrix.GetLength(1) < 2 || matrix.GetLength(1) > 3)
         )
             return -1;
-        if (matrix.GetLength == 2)
+        if (matrix.GetLength(0) == 2)
             return (matrix[0, 0] * matrix[1, 1] - (matrix[1, 0] * matrix[0, 1]));
         return (
             (
