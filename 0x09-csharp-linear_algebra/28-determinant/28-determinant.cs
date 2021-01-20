4cd1901eaee34d255074b503d7bb3fb9d3dc5273
MarcoAnt9 <acalde27@hotmail.com>
2021-01-20 18:22:35 -0500

    Update 28-determinant.cs

--- 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -10,24 +10,23 @@ class MatrixMath
             (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3) ||
             (matrix.GetLength(1) < 2 || matrix.GetLength(1) > 3) ||
             (matrix.GetLength(0) != matrix.GetLength(1))
-
         )
             return -1;
 
-        if (matrix.GetLength(0) == 2)
-            return (matrix[0, 0] * matrix[1, 1] - (matrix[1, 0] * matrix[0, 1]));
+        if (matrix.Rank == 2)
+            return (Math.Round(matrix[0, 0] * matrix[1, 1] - (matrix[1, 0] * matrix[0, 1]), 2));
         return (
+            Math.Round(
             (
             matrix[0, 0] * matrix[1, 1] * matrix[2, 2] +
             matrix[0, 1] * matrix[1, 2] * matrix[2, 0] +
-            matrix[1, 0] * matrix[2, 1] * matrix[0, 2]
-            ) -
+            matrix[1, 0] * matrix[2, 1] * matrix[0, 2])
+            -
             (
             matrix[2, 0] * matrix[1, 1] * matrix[0, 2] +
             matrix[1, 0] * matrix[0, 1] * matrix[2, 2] +
-            matrix[2, 1] * matrix[1, 2] * matrix[0, 0]
-            )
-        );
+            matrix[2, 1] * matrix[1, 2] * matrix[0, 0])
+        , 2));
     }
 }
 
