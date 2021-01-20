3f3393c27652184eb1463ee1db96b9c284f9609d
MarcoAnt9 <acalde27@hotmail.com>
2021-01-20 16:28:06 -0500

    Task 16

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -1,15 +1,16 @@
 ﻿using System;
 
+/// <summary>This is the class object.</summary>
 class MatrixMath
 {
+    /// <summary>This method multiplies a scalar with a matrix.</summary>
     public static double[,] MultiplyScalar(double[,] matrix, double scalar)
     {
-        double[,] bad_mat = { { -1, -1 }, { -1, -1 } };
         if (
             (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3) &&
             (matrix.GetLength(1) < 2 || matrix.GetLength(1) > 3)
         )
-            return (bad_mat);
+            return (new double [,] {{-1}});
         
         for (int i = 0; i < matrix.GetLength(0); i++)
             for (int j = 0; j < matrix.GetLength(1); j++)
