69e3743017d1b9af243e27978265fbed864a8f8a
MarcoAnt9 <acalde27@hotmail.com>
2021-01-20 17:32:58 -0500

    Task 23

---
+++ 0x09-csharp-linear_algebra/25-matrix_transpose/25-matrix_transpose.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+/// <summary>This is the class object.</summary>
+class MatrixMath
+{
+    /// <summary>This method multiplies a scalar with a matrix.</summary>
+    public static double[,] Transpose(double[,] matrix)
+    {
+        if (matrix.Length == 0)
+            return new double[,] { };
+
+        double[,] newMat = new double[matrix.GetLength(1), matrix.GetLength(0)];
+        for (int i = 0; i < newMat.GetLength(0); i++)
+        {
+            for (int j = 0; j < newMat.GetLength(1); j++)
+            {
+                newMat[i, j] = matrix[j, i];
+            }
+        }
+        return (newMat);
+    }
+}
+
