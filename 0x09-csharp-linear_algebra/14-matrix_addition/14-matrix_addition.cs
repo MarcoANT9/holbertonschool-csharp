79ad03db2b618301a2801244ed6f595abb6ae6ef
MarcoAnt9 <acalde27@hotmail.com>
2021-01-20 15:20:36 -0500

    Update 14-matrix_addition.cs

--- 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
+++ 0x09-csharp-linear_algebra/14-matrix_addition/14-matrix_addition.cs
@@ -10,7 +10,12 @@ class MatrixMath
             (matrix1.GetLength(0) != matrix1.GetLength(1)) ||
             (matrix2.GetLength(0) != matrix2.GetLength(1)) ||
             (matrix1.GetLength(0) != matrix2.GetLength(0)) ||
-            (matrix1.GetLength(1) != matrix2.GetLength(1))
+            (matrix1.GetLength(1) != matrix2.GetLength(1)) ||
+            (matrix1.GetLength(0 != 2 || matrix1.GetLength(0) != 3)) ||
+            (matrix2.GetLength(0 != 2 || matrix2.GetLength(0) != 3)) ||
+            (matrix1.GetLength(1 != 2 || matrix1.GetLength(1) != 3)) ||
+            (matrix2.GetLength(1 != 2 || matrix2.GetLength(1) != 3))
+
         )
             return bat_mat;
         int i = 0;
