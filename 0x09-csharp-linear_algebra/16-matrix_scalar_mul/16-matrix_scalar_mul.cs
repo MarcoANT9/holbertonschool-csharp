7f5175ba0f49a30423300c4d0323847567096413
MarcoAnt9 <acalde27@hotmail.com>
2021-01-20 16:09:14 -0500

    Update 16-matrix_scalar_mul.cs

--- 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
+++ 0x09-csharp-linear_algebra/16-matrix_scalar_mul/16-matrix_scalar_mul.cs
@@ -7,7 +7,7 @@ class MatrixMath
         double[,] bad_mat = { { -1, -1 }, { -1, -1 } };
         double[,] bad_mat2 = { { -1, -1, -1 }, { -1, -1, -1 }, {-1, -1, -1} };
         if (
-            (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3) ||
+            (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3) &&
             (matrix.GetLength(1) < 2 || matrix.GetLength(1) > 3)
         )
             if (matrix.Rank == 2)
