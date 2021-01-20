c10fd3268f7282393b9988f0b377824932457cf2
MarcoAnt9 <acalde27@hotmail.com>
2021-01-20 17:43:52 -0500

    Task 26

---
+++ 0x09-csharp-linear_algebra/28-determinant/28-determinant.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+/// <summary>This is the class object.</summary>
+class MatrixMath
+{
+    /// <summary>This method multiplies a scalar with a matrix.</summary>
+    public static double Determinant(double[,] matrix)
+    {
+        if (
+            (matrix.GetLength(0) < 2 || matrix.GetLength(0) > 3) ||
+            (matrix.GetLength(1) < 2 || matrix.GetLength(1) > 3)
+        )
+            return -1;
+        if (matrix.GetLength == 2)
+            return (matrix[0, 0] * matrix[1, 1] - (matrix[1, 0] * matrix[0, 1]));
+        return (
+            (
+            matrix[0, 0] * matrix[1, 1] * matrix[2, 2] +
+            matrix[0, 1] * matrix[1, 2] * matrix[2, 0] +
+            matrix[1, 0] * matrix[2, 1] * matrix[0, 2]
+            ) -
+            (
+            matrix[2, 0] * matrix[1, 1] * matrix[0, 2] +
+            matrix[1, 0] * matrix[0, 1] * matrix[1, 2] +
+            matrix[2, 1] * matrix[1, 2] * matrix[0, 0]
+            )
+        );
+    }
+}
+
