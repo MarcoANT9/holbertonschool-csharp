fa9275028d381534b4e26343a7168fc06c0e6671
MarcoAnt9 <acalde27@hotmail.com>
2021-01-20 20:03:28 -0500

    Update 30-cross_product.cs

--- 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
+++ 0x09-csharp-linear_algebra/30-cross_product/30-cross_product.cs
@@ -6,14 +6,16 @@ class VectorMath
     /// <summary>This method gets the determinant of a matrix.</summary>
     public static double[] CrossProduct(double[] vector1, double[] vector2)
     {
-        if (vector1.Length != 3 && vector2.Length != 3)
-            return new double[] {-1};
-        double[] result = new double[3];
+        double[] result = {-1, -1, -1};
+        if (vector1.Length != 3 || vector2.Length != 3)
+            return result;
+        
         result[0] = vector1[1] * vector2[2] - vector1[2] * vector2[1];
         result[1] = vector1[2] * vector2[0] - vector1[0] * vector2[2];
         result[2] = vector1[0] * vector2[1] - vector1[1] * vector2[0];
-        foreach (double element in result)
-            element = Math.Round(element, 2);
+        
+        for (int i = 0; i < 3; i++)
+            result[i] = Math.Round(result[i], 2);
         return (result);
     }
 }
