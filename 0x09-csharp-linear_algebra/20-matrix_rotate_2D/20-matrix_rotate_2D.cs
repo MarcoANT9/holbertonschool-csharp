e537835ebf6fd049bd1027147449855167267ef1
MarcoAnt9 <acalde27@hotmail.com>
2021-01-21 15:54:09 -0500

    Update 20-matrix_rotate_2D.cs

--- 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
+++ 0x09-csharp-linear_algebra/20-matrix_rotate_2D/20-matrix_rotate_2D.cs
@@ -8,11 +8,11 @@ class MatrixMath
         if (matrix.GetLength(0) != 2 || matrix.GetLength(1) != 2)
             return new double[,] { { -1 } };
 
-        double[,] matRot = {{0, 0}, {0, 0}};
-        matRot[0, 0] = Math.Round(matrix[0,0]*Math.Cos(angle) - matrix[1,0]*Math.Sin(angle), 2);
-        matRot[1, 0] = Math.Round(matrix[0,0]*Math.Sin(angle) + matrix[1,0]*Math.Cos(angle), 2);
-        matRot[0, 1] = Math.Round(matrix[0,1]*Math.Cos(angle) - matrix[1,1]*Math.Sin(angle), 2);
-        matRot[1, 0] = Math.Round(matrix[0,1]*Math.Sin(angle) + matrix[1,1]*Math.Cos(angle), 2);
+        double[,] matRot = { { 0, 0 }, { 0, 0 } };
+        matRot[0, 0] = Math.Round(matrix[0, 0] * Math.Cos(angle) - matrix[0, 1] * Math.Sin(angle), 2);
+        matRot[0, 1] = Math.Round(matrix[0, 0] * Math.Sin(angle) + matrix[0, 1] * Math.Cos(angle), 2);
+        matRot[1, 0] = Math.Round(matrix[1, 0] * Math.Cos(angle) - matrix[1, 1] * Math.Sin(angle), 2);
+        matRot[1, 1] = Math.Round(matrix[1, 0] * Math.Sin(angle) + matrix[1, 1] * Math.Cos(angle), 2);
 
         return (matRot);
 
