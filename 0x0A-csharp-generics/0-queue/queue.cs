303a3048e7ac50a93ed57fe9f67880830a977fc3
MarcoAnt9 <acalde27@hotmail.com>
2021-03-22 14:55:51 -0500

    Task 0: Changed framework and added XML documentation

--- 0x0A-csharp-generics/0-queue/queue.cs
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿
 
+///<summary>This is the Queue Class.</summary>
 class Queue<T>
 {
+    ///<summary>This method returns thr Queue's type.</summary>
     public Type CheckType()
     {
         return (typeof(T));
