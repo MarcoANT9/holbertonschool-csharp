054d15d4ae70f32d643e2a20d7f70407c9466d56
MarcoAnt9 <acalde27@hotmail.com>
2021-03-22 15:00:43 -0500

    Update queue.cs

--- 0x0A-csharp-generics/0-queue/queue.cs
+++ 0x0A-csharp-generics/0-queue/queue.cs
@@ -3,7 +3,7 @@
 ///<summary>This is the Queue Class.</summary>
 class Queue<T>
 {
-    ///<summary>This method returns thr Queue's type.</summary>
+    ///<summary>This method returns the Queue's type.</summary>
     public Type CheckType()
     {
         return (typeof(T));
