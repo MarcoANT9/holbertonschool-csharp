b7588de7de2b25c1569775dcc362b82130a8aa7c
MarcoAnt9 <acalde27@hotmail.com>
2021-03-23 16:46:09 -0500

    Task 3: First Commit

---
+++ 0x0B-csharp-interfaces/3-decorations/3-decorations.cs
@@ -0,0 +1,209 @@
+﻿using System;
+
+//###########################################################################
+//# CLASSES #################################################################
+//###########################################################################
+///<summary>This is the Base class.</summary>
+public abstract class Base
+{
+    //=======================================================================
+    //= Properties ==========================================================
+    //=======================================================================
+
+    // ~ Public ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    /// <summary>Name Property.</summary>
+    public string name { get; set; }
+
+    // ~ Private ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    //=======================================================================
+    //= Classes =============================================================
+    //=======================================================================
+
+    // ~ Public ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    // ~ Private ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    //=======================================================================
+    //= Methods =============================================================
+    //=======================================================================
+
+    // ~ Public ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    /// <summary>ToString Method Override.</summary>
+    public override string ToString()
+    {
+        return $"{this.name} is a {this.GetType().Name}";
+    }
+
+
+    // ~ Private ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    //=======================================================================
+    //= END OF CLASS ========================================================
+    //=======================================================================
+}
+
+//###########################################################################
+//# INTERFACES ##############################################################
+//###########################################################################
+
+///<summary>This interface determines if an object is interactable.</summary>
+public interface IInteractive
+{
+    /// <summary>Method Interact.</summary>
+    void Interact();
+}
+
+///<summary>This interface determines if an object is breakable.</summary>
+public interface IBreakable
+{
+    ///<summary>Property durability.</summary>
+    int durability { get; set; }
+    ///<summary>Method Break.</summary>
+    void Break();
+}
+
+///<summary>This interface determines if an object is collectable.</summary>
+public interface ICollectable
+{
+    /// <summary>Property isCollected.</summary>
+    bool isCollected { get; set; }
+    /// <summary>Method Collect.</summary>
+    void Collect();
+}
+
+
+///<summary>This class correponds to a test object.</summary>
+public class Door : Base, IInteractive
+{
+    //=======================================================================
+    //= Properties ==========================================================
+    //=======================================================================
+
+    // ~ Public ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ///<summary>This assigns the name to the door.</summary>
+    public Door(string name = "Door")
+    {
+        this.name = name;
+    }
+
+    // ~ Private ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    //=======================================================================
+    //= Classes =============================================================
+    //=======================================================================
+
+    // ~ Public ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    // ~ Private ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    //=======================================================================
+    //= Methods =============================================================
+    //=======================================================================
+
+    // ~ Public ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ///<summary>This method allows interaction with objects.</summary>
+    public void Interact()
+    {
+        Console.WriteLine($"You try to open the {name}. It's locked.");
+    }
+
+
+    // ~ Private ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    //=======================================================================
+    //= END OF CLASS ========================================================
+    //=======================================================================
+
+}
+
+
+///<summary>This class correponds to a test object.</summary>
+public class Decoration : Base, IInteractive, IBreakable
+{
+    //=======================================================================
+    //= Properties ==========================================================
+    //=======================================================================
+
+    // ~ Public ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ///<summary>This property determines if an item is a Quest Item.</summary>
+    public bool isQuestItem;
+
+    ///<summary>This property determines if an item is a Quest Item.</summary>
+    public int durability { get; set; }
+
+    ///<summary>Class Constructor.</summary>
+    public Decoration(string name = "Decoration", int durability = 1, bool isQuestItem = false)
+    {
+        this.name = name;
+
+        if (durability < 0)
+            throw new Exception("Durability must be greater than 0");
+
+        this.durability = durability;
+        this.isQuestItem = isQuestItem;
+    }
+
+
+    // ~ Private ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    //=======================================================================
+    //= Classes =============================================================
+    //=======================================================================
+
+    // ~ Public ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    // ~ Private ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    //=======================================================================
+    //= Methods =============================================================
+    //=======================================================================
+
+    // ~ Public ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    ///<summary>This method allows interaction with objects.</summary>
+    public void Interact()
+    {
+        if (durability <= 0)
+            Console.WriteLine($"The {name} has been broken.");
+        else if (isQuestItem)
+            Console.WriteLine($"You look at the {name}. There's a key inside.");
+        else
+            Console.WriteLine($"You look at the {name}. Not much to see here.");
+    }
+
+    ///<summary>This method allows breaking of an object.</summary>
+    public void Break()
+    {
+        durability--;
+
+        if (durability > 0)
+            Console.WriteLine($"Yu hit the {name}. It cracks.");
+        else if (durability == 0)
+            Console.WriteLine($"You smash the {name}. What a mess.");
+        else
+            Console.WriteLine($"The {name} is already broken.");
+    }
+
+
+    // ~ Private ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+
+    //=======================================================================
+    //= END OF CLASS ========================================================
+    //=======================================================================
+
+}
+//###########################################################################
+//######################### END OF FILE #####################################
+//###########################################################################
