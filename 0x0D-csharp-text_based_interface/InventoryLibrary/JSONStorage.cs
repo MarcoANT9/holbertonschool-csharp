56805811fb57ab02ed7d2a914a2feda0ddf82fe6
MarcoAnt9 <acalde27@hotmail.com>
2021-03-29 18:19:36 -0500

    JsonStorage
    Added Methods and namespaces

--- 0x0D-csharp-text_based_interface/InventoryLibrary/JSONStorage.cs
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/JSONStorage.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.IO;
 using System.Text.Json;
+using System.Text.Json.Serialization;
 using System.Collections.Generic;
 
 namespace InventoryLibrary
@@ -22,11 +24,43 @@ namespace InventoryLibrary
         /// This method returns all dictionary.
         /// </summary>
         /// <returns>Objects Dictionary</returns>
-        public object All()
+        public Dictionary<TKey, TValue> All()
         {
             return (this.objects);
         }
 
+        /// <summary>
+        /// Adds a new element to the current dictionary.
+        /// </summary>
+        /// <param name="obj">Objecto to save into de Dict.</param>
+        public void New(object obj)
+        {
+            objects.Add(obj.name + "." + obj.id, obj);
+        }
 
+        /// <summary>
+        /// Serializes the objects dictionary into JSON format and saves it
+        /// into the JSON Storage file (../Storage/inventory_manager.JSON).
+        /// </summary>
+        public void Save()
+        {
+            string jsonString = JsonSerializer.Serialize(objects);
+            string filePath = @"../Storage/inventory_manager.json";
+            using (StreamWriter sw = File.OpenWrite(filePath))
+            {
+                sw.WriteLine(jsonString);
+            }
+        }
+
+        /// <summary>
+        /// Reads from the JSON Storage file (../Storage/inventory_manager.JSON)
+        /// and deserializes the information into the objects Dictionary.
+        /// </summary>
+        public void Load()
+        {
+            string filePath = @"../Storage/inventory_manager.json";
+            string jsonString = File.ReadAllText(filePath);
+            objects = JsonSerializer.deserialize(jsonString);
+        }
     }
 }
