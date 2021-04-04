44e0566ba81f39f6d316edd0f200cb3dace60e05
MarcoAnt9 <acalde27@hotmail.com>
2021-04-04 14:41:35 -0500

    Update BaseClass.cs

--- 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
+++ 0x0D-csharp-text_based_interface/InventoryLibrary/BaseClass.cs
@@ -1,32 +1,32 @@
 ﻿using System;
 
-//namespace InventoryLibrary
-//{
-/// <summary>
-/// This is the base class for the inventory management.
-/// </summary>
-public class BaseClass
+namespace InventoryLibrary
 {
-    public string id { get; }
-    public DateTime date_created { get; }
-    public DateTime date_updated { get; }
-
-    public BaseClass(string id = null, DateTime date_created = default(DateTime), DateTime date_updated = default(DateTime))
+    /// <summary>
+    /// This is the base class for the inventory management.
+    /// </summary>
+    public class BaseClass
     {
-        if (id == null)
-            this.id = Guid.NewGuid().ToString();
-        else
-            this.id = id;
+        public string id { get; }
+        public DateTime date_created { get; }
+        public DateTime date_updated { get; }
 
-        if (date_created == default(DateTime))
-            this.date_created = DateTime.Now;
-        else
-            this.date_created = date_created;
+        public BaseClass(string id = null, DateTime date_created = default(DateTime), DateTime date_updated = default(DateTime))
+        {
+            if (id == null)
+                this.id = Guid.NewGuid().ToString();
+            else
+                this.id = id;
 
-        if (date_updated == default(DateTime))
-            this.date_updated = DateTime.Now;
-        else
-            this.date_updated = date_updated;
+            if (date_created == default(DateTime))
+                this.date_created = DateTime.Now;
+            else
+                this.date_created = date_created;
+
+            if (date_updated == default(DateTime))
+                this.date_updated = DateTime.Now;
+            else
+                this.date_updated = date_updated;
+        }
     }
 }
-//}
